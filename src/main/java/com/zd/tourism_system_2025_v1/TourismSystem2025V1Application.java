f83426c5fdfd363a36b77845fa6412512ec63bb3
Daisy Zhang1 <2790883233@qq.com>
2025-06-05 23:13:09 +0800

    6/5:搭建框架

---
+++ src/main/java/com/zd/tourism_system_2025_v1/TourismSystem2025V1Application.java
@@ -0,0 +1,13 @@
+package com.zd.tourism_system_2025_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismSystem2025V1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourismSystem2025V1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zd/tourism_system_2025_v1/TourismSystem2025V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zd.tourism_system_2025_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismSystem2025V1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
